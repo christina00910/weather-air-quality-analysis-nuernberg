fee2fdb064a3fedcb6a98c22c25d157214f44a55
christina00910 <duerrbeck.christina1@gmail.com>
2026-05-27 18:50:29 +0200

    App Anpassungen
    App Anpassungen

--- 03_app/app.py
+++ 03_app/app.py
@@ -71,6 +71,13 @@ def showEDAPlots (df_prepared, stoff):
             Die Grafik zeigt einen langfristigen Rückgang der PM10-Konzentrationen seit den 1980er-Jahren. Besonders ab den 2000er-Jahren sind deutlich sinkende Werte erkennbar, was auf strengere Umweltauflagen, technische Entwicklungen im Verkehrs- und Industriesektor sowie verbesserte Luftreinhaltemaßnahmen hinweisen könnte. Auch die PM2.5-Werte zeigen seit Beginn der verfügbaren Messreihe ab 2008 eine insgesamt rückläufige Entwicklung.
             """)
 
+    st.markdown(
+        """
+        <div style="margin-top: 50px;"></div>
+        """,
+        unsafe_allow_html=True
+    )
+
     # 2. Saisonales Muster (Jahrzehntvergleich)
     fig_saison = an.calcMeanSaisonYear(df_prepared, stoff)
     if fig_saison: 
@@ -87,7 +94,14 @@ def showEDAPlots (df_prepared, stoff):
             st.caption("""
             Die Grafik zeigt deutliche saisonale Unterschiede der PM10-Konzentrationen im Jahrzehntvergleich. Während in den 1980er- und 2000er-Jahren teilweise höhere Belastungen in einzelnen Herbst- und Wintermonaten auftreten, liegen die Werte im Jahr 2020 insgesamt deutlich niedriger. Dies deutet langfristig auf eine Verbesserung der Luftqualität sowie auf den Einfluss verschärfter Umwelt- und Emissionsmaßnahmen hin.
             """)
-            
+
+        st.markdown(
+        """
+        <div style="margin-top: 50px;"></div>
+        """,
+        unsafe_allow_html=True
+        )
+
     # 3. Rush-Hour-Effekt (Tagesverlauf)
     fig_rush = an.rushHourEffekt(df_prepared, stoff) 
     if fig_rush: 
@@ -101,8 +115,17 @@ def showEDAPlots (df_prepared, stoff):
             Die Grafik zeigt deutliche NO₂-Spitzen während der morgendlichen und abendlichen Hauptverkehrszeiten. Besonders in den Abendstunden steigen die Konzentrationen stark an, was auf erhöhte Verkehrsemissionen sowie eine geringere Verdünnung und Durchmischung der Schadstoffe in der Luft hinweist.
             """)
         elif (stoff == "pm10") :
-            st.info ("CHRISTINA3")
-            
+            st.caption("""
+            Die Grafik zeigt einen typischen Tagesverlauf der PM10- und PM2.5-Konzentrationen mit erhöhten Werten in den frühen Morgen- und Abendstunden. Besonders bei PM10 sind Spitzen während der Hauptverkehrszeiten erkennbar, was auf den Einfluss des Straßenverkehrs und weiterer menschlicher Aktivitäten hinweist. In den Nachmittagsstunden sinken die Konzentrationen dagegen deutlich ab.
+            """)
+
+        st.markdown(
+        """
+        <div style="margin-top: 50px;"></div>
+        """,
+        unsafe_allow_html=True
+        )
+
     # 4. Inversionswetterlage
     fig_inversion = an.inversionswetter(df_prepared, stoff)
     if fig_inversion: 
@@ -112,9 +135,20 @@ def showEDAPlots (df_prepared, stoff):
             Die Grafik zeigt deutlich niedrigere Ozonkonzentrationen während Inversionslagen mit windstillen und wolkenlosen Wetterbedingungen. Dies weist darauf hin, dass Ozon in bodennahen Smogsituationen durch Stickoxide verstärkt abgebaut wird.
             """)
         elif (stoff == "no2") :
-            st.info ("CHRISTINA2")
+            st.caption("""
+            Die niedrigeren NO₂-Konzentrationen bei wolkenlosen und windstillen Wetterlagen deuten auf photochemische Umwandlungsprozesse hin. Unter intensiver Sonneneinstrahlung wird Stickstoffdioxid verstärkt in Ozon umgewandelt, wodurch die gemessenen NO₂-Werte trotz stabiler Wetterlage sinken können.
+            """)
         elif (stoff == "pm10") :
-            st.info ("CHRISTINA3")
+            st.caption("""
+            Die Grafik zeigt, dass die PM10-Konzentrationen während Inversionslagen deutlich höher ausfallen als bei normalen Wetterbedingungen. Besonders bei windstillem und wolkenlosem Wetter können sich Feinstaubpartikel stärker in Bodennähe ansammeln, da die Luft schlechter durchmischt wird. Dadurch steigt die Belastung durch Feinstaub spürbar an.
+            """)
+
+        st.markdown(
+        """
+        <div style="margin-top: 50px;"></div>
+        """,
+        unsafe_allow_html=True
+        )
 
     # 5. Jährliche LQI-Überschreitungen
     fig_exceed = an.getExceedancesPerYear(df_prepared, stoff)
@@ -125,9 +159,20 @@ def showEDAPlots (df_prepared, stoff):
             Die Anzahl der Stunden mit mindestens mäßiger Ozonbelastung ist seit den 1980er-Jahren deutlich angestiegen. Besonders in den letzten Jahren treten erhöhte Ozonkonzentrationen häufiger auf, was auf veränderte klimatische und meteorologische Bedingungen hinweist.
             """)
         elif (stoff == "no2") :
-            st.info ("CHRISTINA2")
+            st.caption("""
+            Die Anzahl der Stunden mit mindestens mäßiger NO₂-Belastung steigt zunächst bis in die frühen 2000er-Jahre an. Anschließend zeigt sich ein deutlicher Rückgang, besonders seit etwa 2010. Dies deutet auf eine langfristige Verbesserung der Luftqualität hin, die unter anderem mit strengeren Emissionsvorgaben und moderneren Fahrzeug- und Heiztechnologien zusammenhängen könnte.
+            """)
         elif (stoff == "pm10") :
-            st.info ("CHRISTINA3")
+            st.caption("""
+            Die Grafik zeigt einen langfristigen Rückgang der Stunden mit mindestens mäßiger Feinstaubbelastung für PM10 und PM2.5. Besonders seit den 2000er-Jahren treten belastete Stunden deutlich seltener auf, was auf Verbesserungen der Luftqualität sowie auf strengere Emissions- und Umweltmaßnahmen hinweisen kann. Trotz einzelner Schwankungen ist insgesamt ein klarer positiver Trend erkennbar.
+            """)
+
+        st.markdown(
+        """
+        <div style="margin-top: 50px;"></div>
+        """,
+        unsafe_allow_html=True
+        )
 
     # 6. Jahreszeit & Werktag/Wochenende (Nimmt die zwei Grafiken sauber entgegen)
     fig_season, fig_weekend = an.analyzeSeasonAndWeekend(df_prepared, stoff)
@@ -138,9 +183,21 @@ def showEDAPlots (df_prepared, stoff):
             Die höchsten Ozonkonzentrationen treten im Sommer und Frühjahr auf, während im Herbst und Winter deutlich niedrigere Werte gemessen werden. Dies verdeutlicht den starken Einfluss von Sonneneinstrahlung und höheren Temperaturen auf die Ozonbildung.
             """)
         elif (stoff == "no2") :
-            st.info ("CHRISTINA2")
+            st.caption("""
+                Die NO₂-Werte sind im Sommer am niedrigsten und im Winter am höchsten. Ursache dafür sind im Sommer stärkere Sonneneinstrahlung, intensivere photochemische Prozesse  und eine bessere Durchmischung der Luft, während sich Schadstoffe im Winter durch geringere Sonneneinstrahlung, stabilere Luftschichten und zusätzliche Emissionen aus dem Heizungs- und Verkehrssektor höhere Konzentrationen anreichern können.
+                """)
         elif (stoff == "pm10") :
-            st.info ("CHRISTINA3")
+            st.caption("""
+            Die Grafik zeigt höhere PM10- und PM2.5-Werte in den Herbst- und Wintermonaten, während die niedrigsten Konzentrationen im Sommer auftreten. Besonders im Winter kann sich Feinstaub durch Heizungen, Verkehr und schlechtere Luftdurchmischung stärker in Bodennähe ansammeln. Im Sommer sorgen günstigere Wetterbedingungen dagegen meist für niedrigere Belastungen.
+            """)
+
+        st.markdown(
+        """
+        <div style="margin-top: 50px;"></div>
+        """,
+        unsafe_allow_html=True
+        )
+
     if fig_weekend: 
         st.pyplot(fig_weekend, use_container_width=False)
         if (stoff == "o3") :
@@ -148,9 +205,13 @@ def showEDAPlots (df_prepared, stoff):
             Die Grafik zeigt höhere durchschnittliche Ozonkonzentrationen an Wochenenden im Vergleich zu Werktagen. Dies könnte auf geringere Stickoxid-Emissionen durch den Straßenverkehr und damit einen verminderten Ozonabbau zurückzuführen sein.
             """)
         elif (stoff == "no2") :
-            st.info ("CHRISTINA2")
+            st.caption("""
+            Die NO₂-Konzentrationen sind an Werktagen höher als am Wochenende. Dies deutet auf den Einfluss des Berufs- und Pendlerverkehrs hin, da Stickstoffdioxid hauptsächlich durch Verbrennungsprozesse im Straßenverkehr entsteht.
+            """)
         elif (stoff == "pm10") :
-            st.info ("CHRISTINA3")
+            st.caption("""
+            Die Grafik zeigt höhere PM10- und PM2.5-Konzentrationen an Werktagen im Vergleich zum Wochenende. Dies deutet darauf hin, dass vor allem Verkehr und andere menschliche Aktivitäten während der Arbeitswoche zur erhöhten Feinstaubbelastung beitragen. Am Wochenende fallen die Konzentrationen dagegen insgesamt etwas niedriger aus.
+            """)
     return
 
 @st.cache_data
@@ -527,7 +588,7 @@ def showTab3 ():
 #######################################################
 @st.fragment
 def showTab4 ():
-    st.header("Korrelationen zwischen Wetter und Schadstoffen – Übersicht")
+    st.header("Korrelationen zwischen Wetter und Schadstoffen")
     kor.korrelation(dfOrginal, stoff_spalte)
     
 #######################################################
@@ -627,6 +688,66 @@ with st.sidebar:
         """,
         unsafe_allow_html=True
     )
+
+# ============================================================
+# TAB-DESIGN ANPASSEN
+# ============================================================
+
+st.markdown("""
+<style>
+
+/* TAB LEISTE */
+.stTabs [data-baseweb="tab-list"] {
+    gap: 0px;
+}
+
+/* EINZELNE TABS */
+button[data-baseweb="tab"] {
+    display: flex !important;
+    align-items: center !important;
+    justify-content: center !important;
+
+    min-height: 54px !important;
+
+    /* WICHTIG */
+    padding: 0 12px !important;
+
+    background-color: transparent !important;
+
+    border: none !important;
+    border-right: 1px solid rgba(255,255,255,0.08) !important;
+
+    transition: all 0.2s ease-in-out;
+}
+
+/* LETZTE LINIE WEG */
+button[data-baseweb="tab"]:last-child {
+    border-right: none !important;
+}
+
+/* TAB TEXT */
+button[data-baseweb="tab"] p {
+    font-size: 15px !important;
+    font-weight: 600 !important;
+
+    margin: 0 !important;
+    text-align: center !important;
+
+    line-height: 1.2 !important;
+}
+
+/* AKTIVER TAB */
+button[data-baseweb="tab"][aria-selected="true"] p {
+    color: #00B5E2 !important;
+}
+
+/* HOVER */
+button[data-baseweb="tab"]:hover {
+    background-color: rgba(255,255,255,0.025) !important;
+}
+
+</style>
+""", unsafe_allow_html=True)
 
 # ============================================================
 # 02 TABS DEFINIEREN & SEITENSTRUKTUR
